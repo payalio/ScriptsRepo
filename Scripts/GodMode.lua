4688b5ca1be71a46293218b32e1e5b3038c96227
payalio <159130208+payalio@users.noreply.github.com>
2025-01-05 13:47:26 +0500

    Create GodMode.lua

---
+++ Scripts/GodMode.lua
@@ -0,0 +1,4 @@
+local Player = Workspace:FindFirstChild(PlayerName, true)
+local Humanoid = Player:FindFirstChild("Humanoid")
+Humanoid.MaxHealth = 9223372036854775808
+Humanoid.Health = 9223372036854775808
